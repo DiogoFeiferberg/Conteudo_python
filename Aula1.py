8a95d33ec286eae949c0724102af8f0d5eb4669f
DIOGO FEIFERBERG <diogo_feiferberg@estudante.sesisenai.org.br>
2025-10-02 11:28:17 -0300

    Explicaçao comando print

---
+++ Aula1.py
@@ -0,0 +1,9 @@
+print("Olá Mundo")
+# Escapei da Maldição
+print("Eu sou o Diogo Feiferberg")
+print('Lógica de Programaçao')
+print('Lógica','de','programaçao',sep="_")
+print('Lógica de programaçao',end='SENAI\n')
+print('Lógica',end=" ")
+print('de',end=" ")
+print('programaçao',end=" ")
